1d5bb0bdfb3fb49deff90b6490b2f4d1c9bd10d3
Abhishek Upperwal <a.upperwal@gmail.com>
2019-06-18 17:38:53 +0530

    simplified RA interface

--- interface/blockchain/blockchain.go
+++ interface/blockchain/blockchain.go
@@ -1,9 +1,10 @@
 package blockchain
 
-type Blockchain interface {
-	// Subscriber specific
-	Subscribe() error
+import peer "github.com/libp2p/go-libp2p-core/peer"
 
-	// Publisher specific
+type RemoteAccess interface {
+	Subscribe() error
 	Publish() error
+	IsPeerSubscribed(peer.ID, string) bool
+	IsPeerAPublisher(peer.ID, string) bool
 }

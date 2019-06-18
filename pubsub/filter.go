33f14656b6a38eec705f0df358c97e92e5e65a43
Abhishek Upperwal <a.upperwal@gmail.com>
2019-06-18 17:40:08 +0530

    filter now takes a remote access object

--- pubsub/filter.go
+++ pubsub/filter.go
@@ -3,22 +3,45 @@ package pubsub
 import (
 	logging "github.com/ipfs/go-log"
 	peer "github.com/libp2p/go-libp2p-core/peer"
+	bc "github.com/upperwal/go-mesh/interface/blockchain"
 )
 
 var log = logging.Logger("fpubsub")
 
-type Filter struct{}
-
-func NewFilter() *Filter {
-	return &Filter{}
+type Filter struct {
+	r bc.RemoteAccess
 }
 
-func (f *Filter) InFilter(p peer.ID) bool {
-	log.Info("Inward peer: ", p)
+func NewFilter(ra bc.RemoteAccess) *Filter {
+	return &Filter{
+		r: ra,
+	}
+}
+
+func (f *Filter) FilterSubscriber(p peer.ID, t string) bool {
+	log.Info("FilterSubscriber")
+
+	if !f.r.IsPeerSubscribed(p, t) {
+		log.Info("Dropping msg to:", p)
+		return false
+	}
+
 	return true
 }
 
-func (f *Filter) OutFilter(p peer.ID) bool {
-	log.Info("Outward peer: ", p)
-	return true
+func (f *Filter) FilterPublisher(p peer.ID, ts []string) []bool {
+	log.Info("FilterPublisher ", p)
+
+	res := make([]bool, len(ts))
+
+	for i, t := range ts {
+		if !f.r.IsPeerAPublisher(p, t) {
+			log.Info("Dropping msg from:", p)
+			res[i] = false
+		} else {
+			res[i] = true
+		}
+	}
+
+	return res
 }

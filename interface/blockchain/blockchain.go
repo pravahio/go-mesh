57936a83b52192c876e5a7c98b391e3259e6df31
Abhishek Upperwal <a.upperwal@gmail.com>
2019-06-23 16:50:12 +0530

    changed interface to RA

--- interface/blockchain/blockchain.go
+++ interface/blockchain/blockchain.go
@@ -3,8 +3,8 @@ package blockchain
 import peer "github.com/libp2p/go-libp2p-core/peer"
 
 type RemoteAccess interface {
-	Subscribe() error
-	Publish() error
-	IsPeerSubscribed(peer.ID, string) bool
-	IsPeerAPublisher(peer.ID, string) bool
+	Subscribe(peer.ID, string) error
+	Publish(peer.ID, string) error
+	IsPeerSubscribed(peer.ID, string) (bool, error)
+	IsPeerAPublisher(peer.ID, string) (bool, error)
 }

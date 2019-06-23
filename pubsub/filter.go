b67832817402853087d37e45216440cc55b0c13a
Abhishek Upperwal <a.upperwal@gmail.com>
2019-06-23 16:50:31 +0530

    now supports caching

--- pubsub/filter.go
+++ pubsub/filter.go
@@ -1,6 +1,7 @@
 package pubsub
 
 import (
+	cache "github.com/bluele/gcache"
 	logging "github.com/ipfs/go-log"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	bc "github.com/upperwal/go-mesh/interface/blockchain"
@@ -10,37 +11,74 @@ var log = logging.Logger("fpubsub")
 
 type Filter struct {
 	r bc.RemoteAccess
+	c cache.Cache
 }
 
 func NewFilter(ra bc.RemoteAccess) *Filter {
 	return &Filter{
 		r: ra,
+		c: cache.New(50).LRU().Build(),
 	}
 }
 
 func (f *Filter) FilterSubscriber(p peer.ID, t string) bool {
 	log.Info("FilterSubscriber")
 
-	if !f.r.IsPeerSubscribed(p, t) {
-		log.Info("Dropping msg to:", p)
-		return false
+	var v bool
+
+	cValue, err := f.c.Get(p.String() + t)
+	if err != nil {
+		v, err = f.r.IsPeerSubscribed(p, t)
+		if err != nil {
+			log.Error("IsPeerSubscribed returned err")
+			return false
+		}
+
+		err = f.c.Set(p.String()+t, v)
+		if err != nil {
+			log.Error(err)
+		}
+	} else {
+		v = cValue.(bool)
 	}
 
-	return true
+	if !v {
+		log.Info("Dropping msg to:", p)
+	}
+
+	return v
 }
 
 func (f *Filter) FilterPublisher(p peer.ID, ts []string) []bool {
 	log.Info("FilterPublisher ", p)
 
+	var v bool
 	res := make([]bool, len(ts))
 
 	for i, t := range ts {
-		if !f.r.IsPeerAPublisher(p, t) {
-			log.Info("Dropping msg from:", p)
-			res[i] = false
+
+		cValue, err := f.c.Get(p.String() + t)
+		if err != nil {
+			v, err = f.r.IsPeerAPublisher(p, t)
+			if err != nil {
+				log.Error("IsPeerAPublisher returned err")
+				res[i] = false
+				continue
+			}
+
+			err = f.c.Set(p.String()+t, v)
+			if err != nil {
+				log.Error(err)
+			}
 		} else {
-			res[i] = true
+			v = cValue.(bool)
 		}
+
+		if !v {
+			log.Info("Dropping msg from:", p, "for topic", t)
+		}
+
+		res[i] = v
 	}
 
 	return res

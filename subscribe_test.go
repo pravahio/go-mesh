02e67354c741b31afc95b83d50969e9c08895c9c
Abhishek Upperwal <a.upperwal@gmail.com>
2019-06-23 16:52:02 +0530

    enhanced err handling

--- subscribe_test.go
+++ subscribe_test.go
@@ -6,30 +6,31 @@ import (
 	"testing"
 	"time"
 
+	logging "github.com/ipfs/go-log"
 	application "github.com/upperwal/go-mesh/application"
+	driver "github.com/upperwal/go-mesh/driver/eth"
 	fpubsub "github.com/upperwal/go-mesh/pubsub"
-
-	// Services
 	bootservice "github.com/upperwal/go-mesh/service/bootstrap"
 	subservice "github.com/upperwal/go-mesh/service/subscriber"
-
-	// Driver
-	driver "github.com/upperwal/go-mesh/driver"
 )
 
 func TestSubscribe(t *testing.T) {
-	/* logging.SetLogLevel("svc-bootstrap", "DEBUG")
+	logging.SetLogLevel("svc-bootstrap", "DEBUG")
 	logging.SetLogLevel("application", "DEBUG")
 	logging.SetLogLevel("svc-subscriber", "DEBUG")
 	logging.SetLogLevel("fpubsub", "DEBUG")
-	logging.SetLogLevel("pubsub", "DEBUG") */
+	logging.SetLogLevel("pubsub", "DEBUG")
+	logging.SetLogLevel("eth-driver", "DEBUG")
 
 	app, err := application.NewApplication(context.Background(), nil, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ethDriver := driver.NewEthDriver()
+	ethDriver, err := driver.NewEthDriver()
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	bservice := bootservice.NewBootstrapService(false, "abc", []string{"/ip4/127.0.0.1/udp/4000/quic/p2p/QmVbcMycaK8ni5CeiM7JRjBRAdmwky6dQ6KcoxLesZDPk9"})
 	subService := subservice.NewSubscriberService(ethDriver)
@@ -45,7 +46,7 @@ func TestSubscribe(t *testing.T) {
 	time.Sleep(3 * time.Second)
 	c, err := subService.SubscribeToTopic("GGN.BUS")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	counter := 0

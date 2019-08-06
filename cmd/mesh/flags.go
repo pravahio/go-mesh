d7db4ac81b371c084d553a768052bc9c3997d40b
Abhishek Upperwal <a.upperwal@gmail.com>
2019-08-06 23:22:19 +0530

    cli for initialising mesh node

---
+++ cmd/mesh/flags.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+const (
+	REMOTE_ACCESS    = "ra"
+	BOOTSTRAP_SERVER = "bs"
+	RENDEZVOUS       = "rnz"
+	ENABLE_PUB       = "en_pub"
+	ENABLE_SUB       = "en_sub"
+	DISABLE_RPC      = "dis_raw_rpc"
+	ENABLE_WEB_RPC   = "web_rpc"
+	DEBUG            = "debug"
+	ACCOUNT          = "account"
+)
+
+var (
+	remoteAccess = cli.StringFlag{
+		Name:  REMOTE_ACCESS,
+		Usage: "remote access url. [http://127.0.0.1:7545]",
+	}
+
+	boostrapServer = cli.StringFlag{
+		Name:  BOOTSTRAP_SERVER,
+		Usage: "bootstrap server multiaddress",
+	}
+
+	bootstrapRendezvous = cli.StringFlag{
+		Name:  RENDEZVOUS,
+		Usage: "Set rendezvous point for bootstrapping",
+	}
+
+	enablePublisher = cli.BoolFlag{
+		Name:  ENABLE_PUB,
+		Usage: "Starts a publisher node.",
+	}
+
+	enableSubscriber = cli.BoolFlag{
+		Name:  ENABLE_SUB,
+		Usage: "Starts a subscriber node.",
+	}
+
+	disableRPC = cli.BoolFlag{
+		Name:  DISABLE_RPC,
+		Usage: "No RPC will be started.",
+	}
+
+	enableWebRPC = cli.BoolFlag{
+		Name:  ENABLE_WEB_RPC,
+		Usage: "enables web based RPC for browsers [http/1.1]",
+	}
+
+	enableDebugging = cli.BoolFlag{
+		Name:  DEBUG,
+		Usage: "Enables debug log printing.",
+	}
+
+	accountFile = cli.StringFlag{
+		Name:  ACCOUNT,
+		Usage: ".msa account file",
+	}
+)
+
+func abs() {
+
+}
---
+++ cmd/mesh/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strconv"
+
+	logging "github.com/ipfs/go-log"
+	"github.com/upperwal/go-mesh/config"
+	mclient "github.com/upperwal/go-mesh/mesh"
+	rpc "github.com/upperwal/go-mesh/rpc/server"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var (
+	app = cli.NewApp()
+	log = logging.Logger("mesh-cli")
+)
+
+func init() {
+	app.Name = "mesh"
+	app.Version = "v0.0.1"
+	app.Description = "go-mesh command line interface"
+	app.Authors = []cli.Author{
+		{Name: "Abhishek Upperwal", Email: "abhishek@soket.in"},
+	}
+	app.Action = mesh
+	app.Flags = []cli.Flag{
+		remoteAccess,
+		boostrapServer,
+		bootstrapRendezvous,
+		enablePublisher,
+		enableSubscriber,
+		disableRPC,
+		enableWebRPC,
+		enableDebugging,
+		accountFile,
+	}
+}
+
+func mesh(ctx *cli.Context) {
+
+	applyLogs(ctx.Bool(DEBUG))
+
+	m, err := mclient.NewMesh(
+		context.Background(),
+		getBootRendz(ctx.String(RENDEZVOUS)),
+		getBootServer(ctx.String(BOOTSTRAP_SERVER)),
+		applyRA(ctx.String(REMOTE_ACCESS)),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	applyNodeType(
+		ctx.Bool(ENABLE_PUB),
+		ctx.Bool(ENABLE_SUB),
+		m,
+	)
+
+	applyRPC(m.Cfg, ctx.Bool(ENABLE_WEB_RPC), ctx.Bool(DISABLE_RPC))
+	rpcs, err := rpc.NewServer(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m.Start()
+	rpcs.Start()
+
+	m.Wait()
+}
+
+func main() {
+	if err := app.Run(os.Args); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
+
+func getBootRendz(b string) config.Option {
+	if b == "" {
+		log.Error("Need a rendezvous string")
+		os.Exit(1)
+	}
+	return func(cfg *config.Config) error {
+		cfg.BootstrapRendezvous = b
+		return nil
+	}
+}
+
+func getBootServer(b string) config.Option {
+	return func(cfg *config.Config) error {
+		cfg.BootstrapNodes = []string{
+			b,
+		}
+		return nil
+	}
+}
+
+func applyRA(s string) config.Option {
+	return func(cfg *config.Config) error {
+		cfg.RemoteAccessURI = s
+		return nil
+	}
+}
+
+func applyRPC(m *config.Config, en_web, dis_raw bool) {
+	if dis_raw {
+		m.RPC = []config.RPCConfig{}
+	}
+
+	if en_web {
+		m.RPC = append(m.RPC, config.RPCConfig{
+			Endpoint: config.RpcURI + strconv.Itoa(config.RpcPort+1),
+			Mode:     "web",
+		})
+	}
+}
+
+func applyNodeType(p bool, s bool, m *mclient.Mesh) {
+	if p {
+		log.Info("Configured as a publisher node.")
+		m.AddPublisher()
+	} else if s {
+		log.Info("Configured as a subscriber node.")
+		m.AddSubscriber()
+	}
+}
+
+func applyLogs(b bool) {
+	if b {
+		logging.SetLogLevel("mesh-cli", "DEBUG")
+		logging.SetLogLevel("rpc-server", "DEBUG")
+		logging.SetLogLevel("application", "DEBUG")
+		logging.SetLogLevel("svc-bootstrap", "DEBUG")
+		logging.SetLogLevel("application", "DEBUG")
+		logging.SetLogLevel("svc-publisher", "DEBUG")
+		logging.SetLogLevel("fpubsub", "DEBUG")
+		logging.SetLogLevel("pubsub", "DEBUG")
+		logging.SetLogLevel("eth-driver", "DEBUG")
+	}
+}

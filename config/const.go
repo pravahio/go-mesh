a8e5b5a7dfd663de239ed0c7b816de99990b6718
Abhishek Upperwal <a.upperwal@gmail.com>
2019-08-22 20:36:37 +0530

    updated bootstrap default

--- config/const.go
+++ config/const.go
@@ -2,7 +2,7 @@ package config
 
 var (
 	bootstrapList = []string{
-		"/ip4/192.168.0.100/udp/4000/quic/p2p/QmVbcMycaK8ni5CeiM7JRjBRAdmwky6dQ6KcoxLesZDPk9",
+		"/ip4/13.234.78.241/udp/4000/quic/p2p/QmVbcMycaK8ni5CeiM7JRjBRAdmwky6dQ6KcoxLesZDPk9",
 	}
 	remoteURI = ""
 	RpcURI    = "127.0.0.1:"

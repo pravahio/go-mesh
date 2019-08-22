2961e8ba0e66eb8a4687ee2c936b026912ae27d0
Abhishek Upperwal <a.upperwal@gmail.com>
2019-08-22 18:41:50 +0530

    making it libp2p quic compat

--- application/quic.go
+++ application/quic.go
@@ -3,22 +3,22 @@ package application
 import (
 	ic "github.com/libp2p/go-libp2p-core/crypto"
 	tpt "github.com/libp2p/go-libp2p-core/transport"
-	ma "github.com/multiformats/go-multiaddr"
-	quictpt "github.com/upperwal/go-libp2p-quic-transport"
+	quictpt "github.com/libp2p/go-libp2p-quic-transport"
+	//ma "github.com/multiformats/go-multiaddr"
 )
 
 var stunServer = "/ip4/40.78.149.141/udp/3000/"
 
 func getQUICTransport(key ic.PrivKey) (tpt.Transport, error) {
-	stunMA, err := ma.NewMultiaddr(stunServer)
+	/* stunMA, err := ma.NewMultiaddr(stunServer)
 	if err != nil {
 		return nil, err
-	}
+	} */
 
-	quicOption := quictpt.TransportOpt{
+	/* quicOption := quictpt.TransportOpt{
 		EnableStun:  false,
 		StunServers: []ma.Multiaddr{stunMA},
-	}
+	} */
 
-	return quictpt.NewTransport(key, quicOption)
+	return quictpt.NewTransport(key)
 }

c5f3b0b9245649f82119a48fde168660fb959a19
Abhishek Upperwal <a.upperwal@gmail.com>
2020-02-11 16:28:42 +0530

    intercept call and add auth + validation

--- rpc/server/server.go
+++ rpc/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -8,11 +10,15 @@ import (
 
 	grpcweb "github.com/improbable-eng/grpc-web/go/grpcweb"
 	logging "github.com/ipfs/go-log"
+	"github.com/pravahio/go-auth-provider/store"
 	config "github.com/pravahio/go-mesh/config"
 	mesh "github.com/pravahio/go-mesh/mesh"
 	rpc "github.com/pravahio/go-mesh/rpc"
 	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -36,17 +42,24 @@ type Server struct {
 	m          *mesh.Mesh
 	lmap       map[string]*LisServer
 	serverOpts []grpc.ServerOption
+	val        *store.Validator
 }
 
 // NewServer creates a new RPC server.
-func NewServer(m *mesh.Mesh) (*Server, error) {
+func NewServer(m *mesh.Mesh, authCrt string) (*Server, error) {
+
+	v, err := store.NewValidator(authCrt)
+	if err != nil {
+		return nil, err
+	}
 
 	s := Server{
 		m:    m,
 		lmap: make(map[string]*LisServer),
+		val:  v,
 	}
 
-	err := s.registerRPC(m.Cfg.RPC)
+	err = s.registerRPC(m.Cfg.RPC)
 	if err != nil {
 		return nil, err
 	}
@@ -84,9 +97,11 @@ func (s *Server) buildServerOptions(config config.RPCConfig) error {
 	}
 	cred, err := credentials.NewServerTLSFromFile(config.CertPath, config.KeyPath)
 	if err != nil {
-		return err
+		return errors.New("Err in Credentials file: " + err.Error())
 	}
 	s.serverOpts = []grpc.ServerOption{
+		grpc.UnaryInterceptor(s.handleUnaryCall),
+		grpc.StreamInterceptor(s.handleStreamCall),
 		grpc.Creds(cred),
 	}
 	return nil
@@ -126,6 +141,37 @@ func (s *Server) CleanUp() {
 			ls.Listener.Close()
 		}
 	}
+}
+
+func (s *Server) handleUnaryCall(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	v, err := s.validateMetadata(ctx)
+	if err != nil || !v {
+		return nil, status.Errorf(codes.InvalidArgument, "missing metadata")
+	}
+	return handler(ctx, req)
+}
+
+func (s *Server) handleStreamCall(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	v, err := s.validateMetadata(ss.Context())
+	if err != nil || !v {
+		return errors.New("Not Valid")
+	}
+	return handler(srv, ss)
+}
+
+func (s *Server) validateMetadata(ctx context.Context) (bool, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return false, status.Errorf(codes.InvalidArgument, "missing metadata")
+	}
+	var sig []string
+	if sig, ok = md["x-signature"]; !ok {
+		return false, errors.New("Missing x-signature")
+	}
+	if len(sig) == 0 {
+		return false, errors.New("x-signature array is length 0")
+	}
+	return s.val.DecodeAndValidate(sig[0]), nil
 }
 
 func buildServer(s *grpc.Server, ty string) RPCServer {
